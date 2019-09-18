d6571150a6ecc7b65522a8009979307c3bfaf524
Shihyen_chen <shihyen_chen@howinvest.com>
2019-09-19 00:42:37 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# download_gmail_attachment
+
+### Reference 
+
+    https://developers.google.com/gmail/api/quickstart/python
+
+### Install the Google Client Library
+```python
+pip install --upgrade google-api-python-client google-auth-httplib2 google-auth-oauthlib
+```
+
+### Use Service Account
+
+1. create a service account: https://console.developers.google.com/iam-admin/serviceaccounts/
+2. in options, create a key: this key is your usual client_secret.json - use it the same way
+3. add owner permission to this service account (Member name = service account ID = service account email ex: thomasapp@appname-201813.iam.gserviceaccount.com
+4. copy the email address of your service account = service account ID
+5. simply go in your browser to the Google sheet you want to interact with
+6. go to SHARE on the top right of your screen
+7. go to advanced settings and share it with email address of your service account ex: thomasapp@appname-201813.iam.gserviceaccount.com
+
---
+++ quickstart.py
@@ -0,0 +1,105 @@
+from __future__ import print_function
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+import urllib.error
+import urllib.request as urllib2
+import base64
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/gmail.readonly','https://mail.google.com/','https://www.googleapis.com/auth/gmail.modify']
+
+def main():
+    """Shows basic usage of the Gmail API.
+    Lists the user's Gmail labels.
+    """
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('gmail', 'v1', credentials=creds)
+
+
+    results = service.users().getProfile(userId='me').execute()
+    print(results)
+    results = service.users().messages().list(userId='me').execute()
+    if not results:
+        print("No mails found.")
+    else:
+        print("Emails:")
+        # https://developers.google.com/gmail/api/v1/reference/users/messages#resource
+        for email in results['messages'][0:100]:
+            email_message = GetMessage(service, 'me', email['id'])
+            print(email['id'])
+            message_id = email['id']
+            for item in email_message['payload']['headers']:
+                # print(item)
+                if item['name'] == 'Subject':
+                    subject = item.get('value')
+                elif item['name'] == 'From':
+                    From = item.get('value')
+
+            # attachments_id = []
+            print(subject, From)
+            if 'parts' in email_message['payload']:
+                for item in email_message['payload']['parts']:
+                    if item['filename']:
+                        print(item['filename'])
+                        print(item['body']['attachmentId'])
+                        attachments_id = item['body']['attachmentId']
+                        GetAttachments(service, 'me', attachments_id, message_id, item['filename'], './attachments/')
+
+def GetMessage(service, user_id, msg_id):
+    try:
+        message = service.users().messages().get(userId=user_id, id=msg_id).execute()
+
+        print ('Message snippet: %s' % message['snippet'])
+
+        return message
+    except urllib2.HTTPError as e:
+        print ('An error occurred: %s' % e)
+
+
+
+def GetAttachments(service, user_id, attachments_id, message_id, filename, store_dir):
+  """Get and store attachment from Message with given id.
+
+  Args:
+    service: Authorized Gmail API service instance.
+    user_id: User's email address. The special value "me"
+    can be used to indicate the authenticated user.
+    msg_id: ID of Message containing attachment.
+    store_dir: The directory used to store attachments.
+  """
+  try:
+    attach_result = service.users().messages().attachments().get(userId=user_id, 
+        id=attachments_id, messageId=message_id).execute()
+    path = ''.join([store_dir, filename])
+    file_data = base64.urlsafe_b64decode(attach_result['data']
+                                                .encode('UTF-8'))
+
+    f = open(path, 'wb')
+    f.write(file_data)
+    f.close()                    
+
+  except urllib2.HTTPError as e:
+    print ('An error occurred: %s' % error)
+
+if __name__ == '__main__':
+    main()
